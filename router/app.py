b930e80ab5060260a624e992d0604c1d73f82aca
Xiaochen <weixc123@gmail.com>
2015-11-01 16:49:26 -0500

    update router

--- router/app.py
+++ router/app.py
@@ -17,31 +17,38 @@ def parse_api(method_name):
 
     if request.method == 'POST':
         student_name = request.args.get('student_name', '')
-        form_data = {"student_name" : student_name}
+        student_id = request.args.get('student_id', '')
+        '''
+        the data of request
+        '''
+        form_data = {"student_name": student_name,
+                     "student_id": student_id}
+
         param = json.dumps(form_data)
         return redirect(url_for('dispaly_redirect_result', data = param), code=307)
     else:
         return "Error: request method is not POST!"
 
-@app.route('/redirect', methods = ["GET", "POST"])
+@app.route('/redirect', methods=["GET", "POST"])
 def dispaly_redirect_result():
     if request.method == 'POST':
         json_data = request.args.get('data', '')
         json_data = json.loads(str(json_data))
 
         student_name = json_data["student_name"]
-        print "Student name :" + student_name
-        
+
+        student_id = json_data["student_id"]
+        if len(student_id) > 0:
+            student_id = int(student_id)
+        else:
+            student_id = 0
+        print "Student name :" + student_name + "Stundet ID: %d" %student_id
+
         # shard based on first letter of student name
         index = (ord(student_name[0]) - 97) / 9
-        return "Student: %s will be direct to student service # %d/3" %(student_name, index)
+        return "Student: %s (ID: %d) will be direct to student service # %d/3" %(student_name, student_id, index)
     else:
         return "Error: request method is not POST!"
-
-
-
-
-
 
 if __name__ == "__main__":
     app.run()
