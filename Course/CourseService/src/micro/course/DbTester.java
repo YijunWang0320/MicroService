e0835a9e6d0aa2c3cdc0694a001dbcce6310bfdb
SongyanHou <housy3@gmail.com>
2015-11-01 22:33:47 -0500

    finish db helper for course CRUD

---
+++ Course/CourseService/src/micro/course/DbTester.java
@@ -0,0 +1,16 @@
+package micro.course;
+
+public class DbTester {
+	public static void main(String args[]) {
+		DatabaseHelper tester = new DatabaseHelper();
+		tester.connect();
+
+		Course c = tester.getCourse(1);
+		System.out.println(c.getCourseId() + "   " + c.getCourseName());
+		c.setCourseName("newnewnewn");
+		tester.updateCourse(c);
+		c = tester.getCourse(1);
+		System.out.println(c.getCourseId() + "   " + c.getCourseName());
+		tester.close();
+	}
+}
