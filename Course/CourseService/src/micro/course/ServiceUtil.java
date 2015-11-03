2959beb44720e539765ac79742658bf4726ca28c
SongyanHou <housy3@gmail.com>
2015-11-03 17:30:10 -0500

    fix redirection

--- Course/CourseService/src/micro/course/ServiceUtil.java
+++ Course/CourseService/src/micro/course/ServiceUtil.java
@@ -5,18 +5,18 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ServiceUtil {
 	/**
@@ -49,31 +49,22 @@ public class ServiceUtil {
 		HttpClient httpclient = HttpClients.createDefault();
 		URI address = new URI("http", null, "129.236.212.69", 5000,
 				"/api/check_exist/", "student_id="+ sid + "&student_name=" + sname, null);
-//		URI address = new URI("http", null, "129.236.212.69", 5003, 
-//				"/student/check_exist/", "data=%7B%22student_id%22%3A+%22wa4738%22%2C+%22student_name%22%3A+%22wangyijun%22%7D", null);
 		HttpPost httppost = new HttpPost(address);
-		httppost.addHeader("Accept", "*/*");
-//		List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-//		params.add(new BasicNameValuePair("student_id", sid));
-//		params.add(new BasicNameValuePair("student_name", sname));
 		InputStream instream = null;
-		System.out.println("In student checker:");
+		String linkRef = null;
+		URI redirect = null;
 		try {
-			//httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
 			HttpResponse response = httpclient.execute(httppost);
 			HttpEntity entity = response.getEntity();
-			System.out.println("Address: " + address + " Code: " + response.getStatusLine().getStatusCode());
-
 			if (entity != null) {
 			    instream = entity.getContent();
 			    StringWriter writer = new StringWriter();
 			    IOUtils.copy(instream, writer, "UTF-8");
 			    String ans = writer.toString();
-			    System.out.println(ans);
-			    if (ans.toUpperCase().equals("TRUE")) {
-			    	return true;
-			    } else {
-			    	return false;
+			    Document doc = Jsoup.parse(ans);
+			    Elements links = doc.getElementsByTag("a");
+			    for(Element link : links) {
+			    	linkRef = link.attr("href");
 			    }
 			}
 		} catch (Exception e) {
@@ -84,8 +75,34 @@ public class ServiceUtil {
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
+		}
+		redirect = new URI(linkRef);
+		httppost = new HttpPost(redirect);
+		HttpResponse finalResponse = null;
+		HttpEntity finalEntity = null;
+		try {
+			finalResponse = httpclient.execute(httppost);
+			finalEntity = finalResponse.getEntity();
+			if (finalEntity != null) {
+				instream = finalEntity.getContent();
+			    StringWriter writer = new StringWriter();
+			    IOUtils.copy(instream, writer, "UTF-8");
+			    String ans = writer.toString();
+			    if (ans.toUpperCase().equals("TRUE")) {
+			    	return true;
+			    } else {
+			    	return false;
+			    }
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 			return false;
 		}
-		return true;
+		return false;
 	}
 }
